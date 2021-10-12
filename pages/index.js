c0079107851633f9f02771ae368934efb153563d
Drew Haines <drewhaines3@gmail.com>
2021-10-12 11:50:29 -0700

    update contact section

--- pages/index.js
+++ pages/index.js
@@ -149,8 +149,8 @@ export default function Home() {
           <h2 class="section_2_subtitle">
             We offer applications for: <br />
             <span>
-              Automotive • Marine • Industrial • Light & Heavy Duty • Vintage &
-              Obsolete
+              Automotive • Marine • Comercial • Industrial • Light & Heavy Duty
+              • Vintage & Obsolete • Hard to Find
             </span>
           </h2>
           <img
@@ -164,10 +164,10 @@ export default function Home() {
             GENERATORS • HEATER MOTORS • REGULATORS • SOLENOIDS • STARTERS •
             STARTER DRIVES • WATER PUMPS • WINDOW LIFT MOTORS • WIPER MOTORS
           </h2>
-          <h2 class="section_2_subtitle_3">
-            Commercial • Industrial • Hard to Find
-          </h2>
           <h2 class="section_2_subtitle_4">We'll do our best be of service</h2>
+          <a href="#contact" class="contact_us">
+            Contact Us
+          </a>
           <h2 class="section_2_subtitle_5">Over 100+ Years Experience</h2>
           <div class="image_grid">
             <img
@@ -239,32 +239,73 @@ export default function Home() {
             <h2 id="contact" class="contact">
               Contact
             </h2>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13417.048924991348!2d-117.1004229!3d32.7853055!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d955c721699f11%3A0x42e425b42e224100!2s4354%20Twain%20Ave%2C%20San%20Diego%2C%20CA%2092120!5e0!3m2!1sen!2sus!4v1633817526511!5m2!1sen!2sus"
-              width="600"
-              height="450"
-              style={{ border: "none" }}
-              allowfullscreen=""
-              loading="lazy"
-            ></iframe>
-            <img
-              src="/outside-3.jpg"
-              alt="aes-workshop"
-              className="aes_workshop"
-            />
-            <img src="/logo.png" alt="aes-workshop" className="aes_workshop" />
-            <div class="aes_info_2">
-              <a
-                href="https://goo.gl/maps/54PEu17PBdUvZsZ39"
-                target="_blank"
-                class="aes_info_item"
+            <Container>
+              <Grid
+                sx={{
+                  gridTemplateColumns: "auto 1fr",
+                  mt: 8,
+                  fontSize: "16px",
+                  margin: "auto",
+                  width: "auto",
+                  p: 5
+                }}
               >
-                4354 Twain Ave, San Diego CA 92120
-              </a>
-              <p class="aes_info_item_2">(619) 282-3207</p>
-              <p class="hours">Mon - Fri | 7:30AM - 4PM</p>
-              <p class="hours_2">Sat - Sun | Closed</p>
-            </div>
+                <Box>Phone</Box>
+                <Box>(619) 282-3207</Box>
+                <Box>Email</Box>
+                <Box>sales@automotiveexchangesupply.com</Box>
+                <Box>Fax</Box>
+                <Box>(619) 677-2790</Box>
+                <Box>Address</Box>
+                <Box>
+                  <a
+                    href="https://goo.gl/maps/54PEu17PBdUvZsZ39"
+                    target="_blank"
+                    className="address-link"
+                  >
+                    4354 Twain Ave, San Diego CA 92120
+                  </a>
+                </Box>
+                <Box>Hours</Box>
+                <Box>
+                  <Box>Mon - Fri | 7:30AM - 4PM</Box>
+                  <Box>Sat - Sun | Closed</Box>
+                </Box>
+              </Grid>
+              <Grid sx={{ gridTemplateColumns: ["1fr", "1fr 1fr"], mt: 0 }}>
+                <Box>
+                  <img
+                    src="/outside-3.png"
+                    alt="aes-workshop"
+                    className="aes-outside"
+                  />
+                </Box>
+                <Box>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13417.048924991348!2d-117.1004229!3d32.7853055!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d955c721699f11%3A0x42e425b42e224100!2s4354%20Twain%20Ave%2C%20San%20Diego%2C%20CA%2092120!5e0!3m2!1sen!2sus!4v1633817526511!5m2!1sen!2sus"
+                    width="100%"
+                    height="100%"
+                    style={{ border: "none", minHeight: "300px" }}
+                    allowfullscreen=""
+                    loading="lazy"
+                  ></iframe>
+                </Box>
+              </Grid>
+              <Grid sx={{ alignItems: "center", justifyContent: "center" }}>
+                <img src="/logo.png" alt="aes-logo" className="aes_logo" />
+                <p className="winner">Be a Winner</p>
+                <p className="rights">All rights reserved AES San Diego</p>
+                <p className="happy">
+                  🏎️
+                  <span>
+                    {`   Happy ${new Date().toLocaleString("en-us", {
+                      weekday: "long"
+                    })}!`}
+                  </span>
+                  🏁
+                </p>
+              </Grid>
+            </Container>
           </div>
         </div>
       </div>
